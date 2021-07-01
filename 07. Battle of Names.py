871533d7bc138ebd3d7891bdfd9f828030b96251
Anton Delchev <77149443+antondelchev@users.noreply.github.com>
2021-07-01 18:33:14 +0300

    Add files via upload

---
+++ 07. Battle of Names.py
@@ -0,0 +1,27 @@
+n = int(input())
+
+even_nums = set()
+odd_nums = set()
+
+for row in range(1, n + 1):
+    name = input()
+    current_sum = 0
+
+    for letter in name:
+        current_sum += ord(letter)
+    after_division = int(current_sum / row)
+
+    if after_division % 2 == 0:
+        even_nums.add(after_division)
+    else:
+        odd_nums.add(after_division)
+
+if sum(even_nums) == sum(odd_nums):
+    union_values = list(odd_nums.union(even_nums))
+    print(*union_values, sep=", ")
+if sum(odd_nums) > sum(even_nums):
+    diff_values = list(odd_nums.difference(even_nums))
+    print(*diff_values, sep=", ")
+if sum(even_nums) > sum(odd_nums):
+    diff_sym_values = list(odd_nums.symmetric_difference(even_nums))
+    print(*diff_sym_values, sep=", ")
